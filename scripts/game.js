ea7f9eb130b2a60c070452613b59fae7a10f333a
Sagittarius87 <Jarik-87@yandex.ru>
2024-01-16 19:22:12 +0300

    Для объекта game добавил свойства running, ctx, width, height, star, score, complexity, sprites, sounds и методы init(), setEvants(), run(), update(), preload(), render(), start(). В init() получил 2d контекст canvas. В start() сделал вызов init().

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,42 @@
-let game = {}
+let game = {
+    running: false,
+    ctx: null,
+    width: null,
+    height: null,
+    star: null,
+    score: null,
+    complexity: null,
+    sprites: {
+        background: null,
+        star1: null,
+    },
+    sounds: {
+        clickStar: null, 
+    },
+    init() {
+        this.ctx = document.getElementById('canvas').getContext('2d')
+        console.log(this.ctx)
+        console.log('init')
+    },
+    setEvants() {
+        console.log('set evants')
+    },
+    run() {
+        console.log('run')
+    },
+    update() {
+        console.log('update')
+    },
+    preload() {
+        console.log('preload')
+    },
+    render() {
+        console.log('render')
+    },
+    start() {
+        this.init()
+        console.log('start')
+    }
+}
+
+game.start()
